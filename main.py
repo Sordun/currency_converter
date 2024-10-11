3b77933f05465923be25d6db28cbe0acc6e5d9e4
Sordun <sordungun@gmail.com>
2024-10-11 12:14:01 +0300

    round replaced on decimal, added description of the function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from decimal import Decimal, ROUND_HALF_UP
+
 import requests
 from fastapi import FastAPI, HTTPException
 
@@ -11,7 +13,19 @@ EXCHANGE_RATE_API_URL = f"https://v6.exchangerate-api.com/v6/{EXCHANGE_API_KEY}/
 
 
 @app.get("/api/rates")
-def get_exchange_rate(from_currency: str = "USD", to_currency: str = "RUB", value: float = 1.0):
+def get_exchange_rate(from_currency: str = "USD", to_currency: str = "RUB", value: float = 1.0) -> dict:
+    """
+    Получает текущий обменный курс и конвертирует сумму из одной валюты в другую.
+
+    :param from_currency: (str) Исходная валюта. По умолчанию "USD".
+    :param to_currency: (str) Целевая валюта. По умолчанию "RUB".
+    :param value: (float) Сумма для конвертации. По умолчанию 1.0.
+    :return: (dict) Результат конвертации с ключом "result" и значением, округленным до двух знаков после запятой.
+
+    :raises HTTPException:
+        - 400, если указана недопустимая целевая валюта.
+        - 500, если произошла ошибка при получении данных o курсах валют или их обработке.
+    """
     try:
         # Получаем данные о курсах валют из внешнего API
         response = requests.get(f"{EXCHANGE_RATE_API_URL}{from_currency}")
@@ -28,9 +42,10 @@ def get_exchange_rate(from_currency: str = "USD", to_currency: str = "RUB", valu
         raise HTTPException(status_code=400, detail="Invalid target currency")
 
     try:
-        exchange_rate = data["conversion_rates"][to_currency]
-        result = round(value * exchange_rate, 2)
+        exchange_rate = Decimal(data["conversion_rates"][to_currency])
+        value_dec = Decimal(value)
+        result = (value_dec * exchange_rate).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
     except KeyError:
         raise HTTPException(status_code=500, detail="Error processing exchange rates data")
 
-    return {"result": result}
+    return {"result": float(result)}
